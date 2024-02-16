22fa3c44171740813b09381cfc92a7cfbcbfb873
Navin <naveenj.iteg2022@ssism.org>
2024-02-16 13:10:51 +0530

    add annotation

--- interviewapp/src/main/java/com/interview/interviewapp/services/StudentServices.java
+++ interviewapp/src/main/java/com/interview/interviewapp/services/StudentServices.java
@@ -11,63 +11,64 @@ import com.interview.interviewapp.dao.StudentRepository;
 import com.interview.interviewapp.entities.Student;
 
 @Service
+@SuppressWarnings("null")
 public class StudentServices {
 
     @Autowired
     private StudentRepository studentRepository;
 
-
     // add student
 
-    public Student addStudent(Student student){
+    public Student addStudent(Student student) {
         return studentRepository.save(student);
     }
 
-    //addstudents
-    public List<Student> addStudents(List<Student>students){
+    // addstudents
+
+    public List<Student> addStudents(List<Student> students) {
         return studentRepository.saveAll(students);
     }
 
-    //get student by id
+    // get student by id
 
-    public Student getStudentbyId(int id){
-       Student student2=null;
-        try{
-        Optional<Student> student=studentRepository.findById(id);
-         student2=student.get();}
-        catch(Exception e){
+    public Student getStudentbyId(int id) {
+        Student student2 = null;
+        try {
+            Optional<Student> student = studentRepository.findById(id);
+            student2 = student.get();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return student2;
-      
+
     }
 
-    //get Students
-    public List<Student>getStudents(){
+    // get Students
+    public List<Student> getStudents() {
         return studentRepository.findAll();
     }
-    //delete student
-   public Student deleteStudentById(int id) {
-    Optional<Student> studentOptional = studentRepository.findById(id);
-    
-    if (studentOptional.isPresent()) {
-        Student student = studentOptional.get();
-        studentRepository.deleteById(id);
-        return student;
-    } else {
-        throw new NoSuchElementException("Student with ID " + id + " not found");
-    }
-}
 
-    
-    //update student
-    public Student updateLevel(Student student,int id){
-        Optional<Student>students=studentRepository.findById(id);
-        Student student1 =students.get();
+    // delete student
+    public Student deleteStudentById(int id) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            studentRepository.deleteById(id);
+            return student;
+        } else {
+            throw new NoSuchElementException("Student with ID " + id + " not found");
+        }
+    }
+
+    // update student
+    public Student updateLevel(Student student, int id) {
+        Optional<Student> students = studentRepository.findById(id);
+        Student student1 = students.get();
         student1.setStudentLevel(student.getStudentLevel());
         studentRepository.save(student1);
         return student1;
-       
+
     }
 
 }
