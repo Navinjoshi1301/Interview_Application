8c3d456d52444db8bce5ebd18b375a74f6687f67
Navin joshi <115449678+Navinjoshi1301@users.noreply.github.com>
2024-06-05 16:55:54 +0530

    Update StudentController.java

--- interviewapp/src/main/java/com/interview/interviewapp/controller/StudentController.java
+++ interviewapp/src/main/java/com/interview/interviewapp/controller/StudentController.java
@@ -1,7 +1,6 @@
 package com.interview.interviewapp.controller;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
